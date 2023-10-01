9fe8b111ad1fe1f2166c2f8508aafae503f34a89
Piyush Kumar <piyushjha5668@gmail.com>
2023-10-01 11:02:35 +0530

    JavaScript Error Fixed

--- index.js
+++ index.js
@@ -4,38 +4,30 @@ const inputCash = document.querySelector("#cash");
 
 const check = document.querySelector(".btn");
 
-console.log(typeof(inputBill.value))
-
 const returnNotes = document.querySelectorAll(".returnNotes");
 
 const errorPrint = document.querySelector(".error");
 
 check.addEventListener('click', checkHandler);
 
-console.log(typeof(inputBill.value))
-
-
-function error(output){
-    if(output == NaN){
-     errorPrint.innerText = "Enter Valid Amount";
-}
-}
-function excessAmount(){
-    if(inputBill.value > inputCash.value){
-        errorPrint.innerText = "Bill Amount should be less than Cash Amount"
-    }
-}
 
 function checkHandler() {
-    if(inputBill.value > inputCash.value){
+    var billAmount = parseFloat(inputBill.value);
+    var cashAmount = parseFloat(inputCash.value);
+
+
+    if(billAmount > cashAmount){
         errorPrint.innerText = "Bill Amount should be less than Cash Amount"
+        return;
     }
 
-
-    // excessAmount();
+    if(isNaN(billAmount) || isNaN(cashAmount)){
+        errorPrint.innerText = "Enter Valid Amount";
+        return;
+    }
     const notesDenomination = [2000, 500, 100, 50, 20, 10, 5, 1];
 
-    var returnValue = inputCash.value - inputBill.value;
+    var returnValue = cashAmount - billAmount;
     
     for (var i = 0 ; i <= notesDenomination.length; i++) {
 
